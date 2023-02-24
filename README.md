a9422277c96b676da0b61a16d422815cc5bdbdf8
escotoj <j.escoto14@gmail.com>
2023-02-24 12:58:46 -0800

    finalized the prompts

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+# Module 3 Challenge 
+## What was done
 created a html, css, and js file for the module 3 challenge. 
 cloned the repo, and have made a few updates. 
 Created the structure to the page, added button and input text box. 
-must include a promp when we click the button - X
-select the critera for the password
-set the legnth of the password as part of the
+must include a promp when we click the button
+select the critera for the password by adding the different prompts
+used if statements and else statements
+used for-loop to cross check each criteria. 
+
+## Things Learned
+The use of the for-loop and if statements
+further undertstanding of the git hub repo process
+more practice with html and CSS elements
+
+## Struggles
+Using logic to find to set up the if and for-loop statements.
+Making variables that made sense and didnt confuse me
--- app.js
+++ app.js
@@ -1,84 +1,80 @@
 // var password = prompt("Enter in the password");
 //         if (password=="1234") {
 //             //location = "media.html"
-          
-        // else 
-        // {
-        //   document.getElementById("msg").value = "test";  
-        //   //location = "error.html"
-        // }
-       
-        // {
-        //     const password = parseInt(prompt(“please input password”))
-        // }
 
-        
-// 
+// else
+// {
+//   document.getElementById("msg").value = "test";
+//   //location = "error.html"
+// }
 
-// console.log('hi');
-// let wins = 0;
-// let losses = 0;
-// let ties = 0;
+// {
+//     const password = parseInt(prompt(“please input password”))
+// }
 
-// const cpuChoices = [
-//   "r",
-//   "p",
-//   "s",
-// ];
+//
 
-// const playGame = function () {
-//     const playerChoice = prompt('Choose R, P, or S').toLowerCase();
-//     if (playerChoice !== 'r' && playerChoice !== 'p' && playerChoice !== 's') {
-//       alert("You must choose a valid option");
-//       return;
-//     }
-//     // generate random between 0 ~ 2
-//     const randomNum = Math.floor(Math.random() * 3);
-//     // choose option for computer
-//     const cpuChoice = cpuChoices[randomNum];
-//     // first we handle winning conditions
-//     console.log(cpuChoice, playerChoice);
-//     // if begins
-//     if (
-//       (cpuChoice === 'r' && playerChoice === 'p') ||
-//       (cpuChoice === 'p' && playerChoice === 's') ||
-//       (cpuChoice === 's' && playerChoice === 'r')
-//     ) {
-//       wins++;
-//       console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
-//     } else if (
-//       (playerChoice === 'r' && cpuChoice === 'p') ||
-//       (playerChoice === 'p' && cpuChoice === 's') ||
-//       (playerChoice === 's' && cpuChoice === 'r')
-//     ) {
-//       losses++;
-//       console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
-//     } else {
-//       ties++;
-//       console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
-//     }
-//   //  if ends
-
-//     // ask if they want to play again
-//     const playAgain = confirm('Do you want to play again?');
-
-//     if (playAgain) {
-//       playGame();
-//     }
-    
 //   }
-;
 //  FIRST ATTEMPT USING TIPS AND CODE FROM CLASS
-        function promptMe(){
-        var userPassword = prompt("Please provide a Password");
-        alert (userPassword);
-        // if block should be here userPassword !== userPassword.length <= 8 
-var passwordLength = prompt("Please make password at least 8 characters long");
-        alert (userPassword);
-        var passwordChar = prompt("Please include at least one of the following; lowercase, uppercase, numeric, and/or special characters");
-        alert (userPassword);
-        // work with the || sign, looking in to using split method
+var listChar = ["@", "$", "!", "<", ">", "?", "&", "%", "@"]
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var UpperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+var finalPassword = []
+
+
+function promptMe() {
+  var userPassword = prompt(
+    "Please chose a password lenght between 8 and 128 characters long"
+  );
+
+  if (userPassword < 8 || userPassword > 128) {
+    alert("Your password does dont meet criteria");
+    promptMe();
+  } else {
+    var specialChar = confirm("Do you want to include special characters");
+    var lowercaseChar = confirm("Do you wish to add a lower case letter");
+    var uppercaseChar = confirm("Do you wish to add a upper case letter");
+    var numberChar = confirm("Do you wish to add a number");
+    if (specialChar || lowercaseChar || uppercaseChar || numberChar) {
+        if (specialChar) { 
+            optionChar = optionChar.concat(listChar)
+        }
+        if (numberChar) {
+            optionChar = optionChar.concat(numChar)
+        }
+        if (lowercaseChar) {
+            optionChar = optionChar.concat(UpperChar)
+        }
+    } else {
+      alert("Your password does dont meet criteria");
+    }
+  }
+for (var i = 0; i < userPassword; i++) {
+    var finalPassword = (listChar[i] + lowerChar[i] +
+    UpperChar + numChar[i]);
+    alert(finalPassword)
 }
+// var password = document.getElementById("password");
+// var generatedPass = alert(optionChar)
+}
+
+// var specialChar = ["@", "$", "!", "<", ">", "?", "&", "%", "@"]
+// var lowercaseChar = [""]
+// console.log("user password: ", userPassword)
+
+// prompt(
+//     "Please include at least one of the following; lowercase, uppercase, numeric, and/or special characters"
+//   );
+
+// alert (userPassword);
+
+// var passwordChar = prompt("Please include at least one of the following; lowercase, uppercase, numeric, and/or special characters");
+// alert (userPassword);
+
+// work with the || sign, looking in to using split method
+// }
 
 // if (passwordLength => userPassword.length) {
 //     console.log("Meets Criteria");
@@ -87,54 +83,45 @@ var passwordLength = prompt("Please make password at least 8 characters long");
 //     prompt("Please make password at least 8 characters long");
 //   }
 
-
-
-// 2nd ATTEMPT AFTER READING SECTION 17 
+// 2nd ATTEMPT AFTER READING SECTION 17
 // var userPassword = length(8)
-var passwordLength = length[8]
-var passwordChar = ("@", "&", "$", "*", "#") 
 
-function meetsCriteria(userPassword, passwordLength) {
-    if (x === y) {
-      console.log('Meets Criteria');
-    } else if (x == y) {
-      console.log('ehh');
-    } else {
-      console.log('Please make password at least 8 characters long');
-    }
-    return;
-  }
+// var passwordChar = ("@", "&", "$", "*", "#")
 
-  function meetsCriteria2(userPassword, passwordChar) {
-    if (x === y) {
-      console.log('Meets Criteria');
-    } else if (x == y) {
-      console.log('ehh');
-    } else {
-      console.log('Please use a special character from the list above');
-    }
-    return;
-  }
+// function meetsCriteria(userPassword, passwordLength) {
+//     if (x === y) {
+//       console.log('Meets Criteria');
+//     } else if (x == y) {
+//       console.log('ehh');
+//     } else {
+//       console.log('Please make password at least 8 characters long');
+//     }
+//     return;
+//   }
 
-
-
-
-
+//   function meetsCriteria2(userPassword, passwordChar) {
+//     if (x === y) {
+//       console.log('Meets Criteria');
+//     } else if (x == y) {
+//       console.log('ehh');
+//     } else {
+//       console.log('Please use a special character from the list above');
+//     }
+//     return;
+//   }
 
 // var passwordCheck = (passwordLength => text.length)
 
 // console.log(passwordCheck)
 
-
 // if { const userPassword !== function text.passwordLength }
-// if { 
+// if {
 //     console.log(userPassword == function
 // }
 
-       
 //         const passwordCrit = function () {
 //             const passwordLength = prompt('Make password at least 8 characters long').toLowerCase();
-            // if (playerChoice !== 'r' && playerChoice !== 'p' && playerChoice !== 's') {
-            //   alert("You must choose a valid option");
-            //   return;
-            // }
+// if (playerChoice !== 'r' && playerChoice !== 'p' && playerChoice !== 's') {
+//   alert("You must choose a valid option");
+//   return;
+// }
